ef3048f6e58954977211411060433177835706e3
Brett Bejcek <bbejcek@Bretts-MacBook-Air.local>
2016-03-23 19:35:49 -0400

    Added Exploratory Analysis R Script

---
+++ Exploratory Analysis.R
@@ -0,0 +1,11 @@
+## ======================================================================
+## Stat 3302 Project:
+##
+##   Exploratory data analysis, model building, selection, 
+##   and diagnostics around the Barry Bonds 2001 Plate
+##   Appearances. 
+##
+##   http://www.amstat.org/publications/jse/datasets/bonds2001.txt
+##
+##   Brett Bejcek, Kyle Voytovich, Last updated March 2016
+## ======================================================================
