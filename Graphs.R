b66388a2a88aec1f8be9bfbe05716e784138dd5d
Brett Bejcek <bejcek.2@osu.edu>
2016-04-17 10:16:46 -0400

    Refined first graph

--- Graphs.R
+++ Graphs.R
@@ -2,4 +2,5 @@
 
 onBaseGraph <- read.csv("~/Stat 3302/Stat 3302 Project/onBaseGraph.csv")
 dat.m <- melt(onBaseGraph,id.vars = "onbase")
-ggplot(dat.m,aes(x = onbase, y = value)) + geom_bar(aes(fill = variable),position = "dodge", stat="identity") + xlab("Number of Players on Base") + ylab("Count")
+ggplot(dat.m,aes(x = onbase, y = value)) + geom_bar(aes(fill = variable),position = "dodge", stat="identity") + xlab("\nNumber of Runners on Base") + ylab("Count\n")+ theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=20), legend.text=element_text(size=20)) + guides(fill=guide_legend(title=NULL)) + scale_fill_manual(values=c("#56B4E9", "#F11E1E"))
